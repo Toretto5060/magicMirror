c27eed0c2d1a14de35380aeebde0238db6ca3f5c
张磊 <zhanglei@inesadt.com>
2018-10-10 18:33:35 +0800

    新增欢迎页、引入elementUi和mintUi

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,83 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <transition name="el-fade-in-linear">
+        <div v-if="isShow" class="transition-box await">
+          <p class="Welcomes">{{Welcomes}}</p>
+          <p class="Starting">{{Starting}}</p>
+        </div>
+    </transition>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return {
+      isShow:true,
+      Welcomes:"",
+      Starting:""
+    }
+  },
+  mounted() {
+    if(localStorage.getItem("msg")){
+      
+    }else{
+      this.Welcomes="6666";
+      this.Starting="魔镜系统正在启动中...";
+    }
+    setTimeout(() => {
+      this.isShow = false
+    }, 3000);
+  },
+  methods:{
+
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+body,html,*{
+  margin: 0;
+  padding: 0;
 }
+#app {
+  font-family:"草书";//楷体
+  width: 100%;
+  height: 100%;
+  .await{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    color:#eee;
+    background-color: black;
+    z-index: 99999999999;
+    .Welcomes{
+      width: 200px;
+      height: 80px;
+      text-align: center;
+      position: absolute;
+      top:0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      margin: auto;
+    }
+    .Starting{
+      width: 200px;
+      height: 80px;
+      text-align: center;
+      position: absolute;
+      // top:0;
+      left: 0;
+      right: 0;
+      bottom: 50px;
+      margin:auto;
+    }
+  }
+}
+
 </style>
---
+++ src/components/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+
+    }
+  },
+  mounted() {
+  
+  },
+  methods:{
+    await(params) {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: #000;
+  }
+  
+</style>
