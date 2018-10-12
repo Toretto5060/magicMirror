c8dd9dfe93c0051918a3b4d4928dcef03f7de507
张磊 <zhanglei@inesadt.com>
2018-10-12 18:26:52 +0800

    no message

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,13 @@
 import axiosIns from './resouce.js';
+// import qs from 'qs';
 
 let city = "410100"; //郑州市辖区
 let key = "e7aceb8dac3c639e2fe8a6301926ebb3";
 
-let weather = 'http://restapi.amap.com/v3/weather/weatherInfo?city=' + city + '&key=' + key + '&extensions=all'
+// let weather = 'http://restapi.amap.com/v3/weather/weatherInfo?city=' + city + '&key=' + key + '&extensions=all'
+let weather = 'http://t.weather.sojson.com/api/weather/city/101030100'
 
 export const getWeather = params => {
-  return axiosIns.get( weather , params)
+  return axiosIns.get(weather, params) //qs.stringify(params)
 }
-
+  
--- src/components/index.vue
+++ src/components/index.vue
@@ -10,17 +10,42 @@ export default {
   name: 'index',
   data () {
     return {
+      YTD:'',
+
 
     }
   },
   mounted() {
     this.getWeather();
+    this.getData(); 
   },
   methods:{
+    getData(){
+      let that = this;
+      setInterval(function () {
+        let nowDate = new Date;
+        let years= nowDate.getFullYear();    //获取完整的年份(4位,1970-????)
+        let month=  nowDate.getMonth()+1;       //获取当前月份(0-11,0代表1月)
+        let date=nowDate.getDate();        //获取当前日(1-31)
+        let week=nowDate.getDay();         //获取当前星期X(0-6,0代表星期天)
+        let hours= nowDate.getHours();       //获取当前小时数(0-23)
+        let minutes= nowDate.getMinutes();     //获取当前分钟数(0-59)
+        let seconds= nowDate.getSeconds();     //获取当前秒数(0-59)
+        function zero(num) {
+          if(num<10){
+            num = "0"+num;
+          }
+          return num;
+        }
+        that.YTD = years+"年"+zero(month)+"月"+zero(date)+"日";
+        console.log(that.YTD)
+
+      },1000)
+    },
     getWeather(){
-       getWeather().then(body=>{
-         console.log(body)
-       })
+      //  getWeather().then(body=>{
+      //    console.log(body)
+      //  })
     }
   }
 }
